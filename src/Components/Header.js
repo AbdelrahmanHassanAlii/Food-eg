1b8e4eb9f8c4c2ec525afd0b83707f6f40f8baf8
venom <abdo30201080101694@gmail.com>
2024-02-09 21:24:52 +0200

    handle basic header

---
+++ src/Components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../Css/header.css";
+
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="container">
+        <div className="brand-name"><span>OTLOP</span>-eg</div>
+        <div className="links">
+          <div>link-1</div>
+          <div>link-1</div>
+          <div>link-1</div>
+          <div>link-1</div>
+        </div>
+      </div>
+    </div>
+  );
+}
