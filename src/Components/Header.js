159d9e6d174ca9c6ce63e5886e661293026854c0
venom <abdo30201080101694@gmail.com>
2024-02-13 17:33:47 +0200

    handle nesting and remove duplicate

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,19 @@
 import React from "react";
 import "../Css/header.css";
 
-
 export default function Header() {
   return (
     <div className="header">
       <div className="container">
-        <div className="brand-name"><span>OTLOP</span>-eg</div>
-        <div className="links">
-          <div>link-1</div>
-          <div>link-1</div>
-          <div>link-1</div>
-          <div>link-1</div>
+        <div className="brand-name">
+          <span>FOOD</span>-eg
         </div>
+        {/* <div className="links">
+          <div>link-1</div>
+          <div>link-1</div>
+          <div>link-1</div>
+          <div>link-1</div>
+        </div> */}
       </div>
     </div>
   );
